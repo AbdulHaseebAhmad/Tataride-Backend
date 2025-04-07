f876e06fa851c1fbfa7501a2bcf26bbca0e2fe20
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2025-04-07 22:32:13 +0100

    Second CORS Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://tataride.web.app"],
+    origin: ["http://localhost:5173","https://tataride.web.app","https://tataride.web.app/place-order"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
