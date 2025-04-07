51864c3b67b10091351ae1f1cef69423ecfb15b2
Abdul Haseeb <abdull.haseebkhan@gmail.com>
2025-04-07 22:30:49 +0100

    Cors Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://tataride.web.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
